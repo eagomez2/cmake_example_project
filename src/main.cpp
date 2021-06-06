72601d3e7bc6ca30003f103628f16aba65864f3c
eagomez <esteban.gomez@ug.uchile.cl>
2021-06-06 03:23:17 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include "Config.h"
+
+int main(int argc, char* argv[]) {
+        printf("Running: %s %i.%i\n", argv[0], VERSION_MAJOR, VERSION_MINOR);
+        printf("Hello, Cmake!\n");
+        return 0;
+}
